cef608a0f165f64b7a2c0b0a34c121b84f7bb4c7
marjana07 <mykytovych007@gmail.com>
2019-11-07 18:12:17 +0200

    solved incorrect pattern
    Solved problem with first letter in second word

--- src/main/java/L1_2/Main.java
+++ src/main/java/L1_2/Main.java
@@ -1,21 +1,22 @@
 package com.company;
+
 import java.util.Scanner;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String pattern = "[A-Z]+[a-z]*\\s[A-Z]+[a-z]*";
+        String pattern = "([A-Z][a-z]+)(\\s)([A-Z])([a-z]+)";
         String input = sc.nextLine();
         sc.close();
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(input);
-        if (m.matches()){
+        if (m.matches()) {
             System.out.println(input);
-        }
-        else
+        } else
             System.out.println("wrong format");
     }
 }
