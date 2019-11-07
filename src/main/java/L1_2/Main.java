40206e1081260a0955f65b5af0534f8ba0202771
marjana07 <mykytovych007@gmail.com>
2019-11-07 22:24:28 +0200

    add method checkInput

--- src/main/java/L1_2/Main.java
+++ src/main/java/L1_2/Main.java
@@ -7,16 +7,22 @@ import java.util.regex.Pattern;
 
 public class Main {
 
+    static String pattern = "(^[A-Z][a-z]+)(\\s)([A-Z])([a-z]+$)";
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String pattern = "([A-Z][a-z]+)(\\s)([A-Z])([a-z]+)";
+        System.out.println("Please, enter your name and surname ");
+        checkInput(sc);
+    }
+
+    public static void checkInput(Scanner sc) {
         String input = sc.nextLine();
         sc.close();
         Pattern p = Pattern.compile(pattern);
         Matcher m = p.matcher(input);
         if (m.matches()) {
-            System.out.println(input);
+            System.out.println("Hello " + input);
         } else
-            System.out.println("wrong format");
+            System.out.println("Wrong format");
     }
 }
