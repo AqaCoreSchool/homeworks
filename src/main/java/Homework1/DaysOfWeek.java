f9a0bfd75f9457488c26715ea65a10333f2cfc54
Mykola111 <odkolya@gmail.com>
2019-11-08 18:37:19 +0200

    changes for task1

--- src/main/java/Homework1/DaysOfWeek.java
+++ src/main/java/Homework1/DaysOfWeek.java
@@ -1,20 +1,54 @@
 package Homework1;
 
 public enum DaysOfWeek {
-    SUNDAY("Sunday"),
-    MONDAY("Monday"),
-    TUESDAY("Tuesday"),
-    WEDNESDAY("Wednesday"),
-    THURSDAY("Thursday"),
-    FRIDAY("Friday"),
-    SATURDAY("Saturday");
+    SUNDAY {
+        @Override
+        DaysOfWeek nextDay() {
+            return MONDAY;
+        }
+    },
+    MONDAY {
+        @Override
+        DaysOfWeek nextDay() {
+            return TUESDAY;
+        }
+    },
+    TUESDAY {
+        @Override
+        DaysOfWeek nextDay() {
+            return WEDNESDAY;
+        }
+    },
+    WEDNESDAY {
+        @Override
+        DaysOfWeek nextDay() {
+            return THURSDAY;
+        }
+    },
+    THURSDAY {
+        @Override
+        DaysOfWeek nextDay() {
+            return FRIDAY;
+        }
+    },
+    FRIDAY {
+        @Override
+        DaysOfWeek nextDay() {
+            return SATURDAY;
+        }
+    },
+    SATURDAY {
+        @Override
+        DaysOfWeek nextDay() {
+            return SUNDAY;
+        }
+    };
 
     private String day;
 
-    DaysOfWeek(String day) {
-        this.day = day;
+
+    DaysOfWeek() {
     }
-    public String day() {
-        return this.day;
-    }
+
+    abstract DaysOfWeek nextDay();
 }
