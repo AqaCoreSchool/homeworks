a42bc5d5736ec4dce719f07abe2c1df1cca58c94
Andrii Rybka <r.andy@ukr.net>
2019-11-10 14:04:25 +0200

    Used abstract method

--- src/main/java/rybka/WeekDays.java
+++ src/main/java/rybka/WeekDays.java
@@ -1,15 +1,52 @@
-package rybka;
+package com.rybka.homework1;
 
 import java.util.Scanner;
 
-enum DaysOfWeek{
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY
+enum DaysOfWeek {
+    MONDAY {
+        @Override
+        Enum nextDay() {
+            return TUESDAY;
+        }
+    },
+    TUESDAY {
+        @Override
+        Enum nextDay() {
+            return WEDNESDAY;
+        }
+    },
+    WEDNESDAY {
+        @Override
+        Enum nextDay() {
+            return THURSDAY;
+        }
+    },
+    THURSDAY {
+        @Override
+        Enum nextDay() {
+            return FRIDAY;
+        }
+    },
+    FRIDAY {
+        @Override
+        Enum nextDay() {
+            return SATURDAY;
+        }
+    },
+    SATURDAY {
+        @Override
+        Enum nextDay() {
+            return SUNDAY;
+        }
+    },
+    SUNDAY {
+        @Override
+        Enum nextDay() {
+            return MONDAY;
+        }
+    };
+
+    abstract Enum nextDay();
 }
 
 public class WeekDays {
@@ -19,16 +56,31 @@ public class WeekDays {
         System.out.print("Enter any day please: ");
         String day = scanner.next();
 
-        switch(day.toUpperCase()){
-            case "MONDAY": System.out.println("The next day is "+DaysOfWeek.TUESDAY); break;
-            case "TUESDAY":  System.out.println("The next day is "+DaysOfWeek.WEDNESDAY); break;
-            case "WEDNESDAY":  System.out.println("The next day is "+DaysOfWeek.THURSDAY); break;
-            case "THURSDAY":  System.out.println("The next day is "+DaysOfWeek.FRIDAY); break;
-            case "FRIDAY": System.out.println("The next day is "+DaysOfWeek.SATURDAY); break;
-            case "SATURDAY":  System.out.println("The next day is "+DaysOfWeek.SUNDAY); break;
-            case "SUNDAY":  System.out.println("The next day is "+DaysOfWeek.MONDAY); break;
-            default: System.out.println("There is no day like this!"); break;
+        switch (day.toUpperCase()) {
+            case "MONDAY":
+                System.out.println("The next day is " + DaysOfWeek.MONDAY.nextDay());
+                break;
+            case "TUESDAY":
+                System.out.println("The next day is " + DaysOfWeek.TUESDAY.nextDay());
+                break;
+            case "WEDNESDAY":
+                System.out.println("The next day is " + DaysOfWeek.WEDNESDAY.nextDay());
+                break;
+            case "THURSDAY":
+                System.out.println("The next day is " + DaysOfWeek.THURSDAY.nextDay());
+                break;
+            case "FRIDAY":
+                System.out.println("The next day is " + DaysOfWeek.FRIDAY.nextDay());
+                break;
+            case "SATURDAY":
+                System.out.println("The next day is " + DaysOfWeek.SATURDAY.nextDay());
+                break;
+            case "SUNDAY":
+                System.out.println("The next day is " + DaysOfWeek.SUNDAY.nextDay());
+                break;
+            default:
+                System.out.println("There is no day like this!");
+                break;
         }
     }
 }
-
