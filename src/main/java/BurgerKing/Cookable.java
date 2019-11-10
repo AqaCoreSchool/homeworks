2a17c3ae0d940ad5ce003c1ed21a7bac374c2592
nkfortest <nazariy.kushnir@gmail.com>
2019-11-10 12:20:42 +0200

    BurgerKing project base logic

---
+++ src/main/java/BurgerKing/Cookable.java
@@ -0,0 +1,6 @@
+package BurgerKing;
+
+public interface Cookable {
+
+    public void cooking();
+}
---
+++ src/main/java/BurgerKing/Main.java
@@ -0,0 +1,7 @@
+package BurgerKing;
+
+public class Main {
+    public static void main(String[] args) {
+//        to be done...
+    }
+}
