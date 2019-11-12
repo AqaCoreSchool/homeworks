b9516e81931475efd87f5f52bd259ac18909ac17
Ilya <47056258+IlyaGorod@users.noreply.github.com>
2019-11-12 17:19:40 +0200

    first

---
+++ hotelBooking/src/Payment.java
@@ -0,0 +1,12 @@
+public class Payment {
+
+
+     public void cashToPay(){
+        System.out.println("cash");
+    }
+
+     public void cashlessToPay(){
+        System.out.println("cashless");
+    }
+
+}
---
+++ hotelBooking/src/PricePerRoom.java
@@ -0,0 +1,4 @@
+public interface PricePerRoom {
+
+     void getPriceRoom();
+}
