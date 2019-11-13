5bd6c665cb368a1b93d0f618d2aafe611df57b50
51stRoR <51sttestinrror@gmail.com>
2019-11-13 09:47:17 +0200

    change main test flow change conditions in check isOneOf, isAll, isNone methods

--- src/main/java/com/burgerking/Main.java
+++ src/main/java/com/burgerking/Main.java
@@ -8,17 +8,18 @@ public class Main {
         daymenu.addProduct(new Food("Cheeseburger", FoodType.BURGER, 350, 77.00, "regular"));
         daymenu.addProduct(new Food("Big Mac", FoodType.BURGER, 550, 110.00, "big"));;
         daymenu.addProduct(new Food("Fries", FoodType.FRIED, 400, 70.00, "small"));
-        daymenu.addProduct(new Food("Hamburger", FoodType.BURGER, 320, 56.00, "regular"));
+        daymenu.addProduct(new Food("Hamburger", FoodType.BURGER, 320, 77.01, "regular"));
         daymenu.addProduct(new Food("Americano", FoodType.COFFEE, 80, 29.00, "medium"));
-        daymenu.addProduct(new Food("Fries&cheese", FoodType.FRIED, 600, 100.45, "big"));
+        daymenu.addProduct(new Food("Fries&cheese", FoodType.FRIED, 600, 44.49, "big"));
         daymenu.addProduct(new Food("Espresso", FoodType.COFFEE, 65, 25.00, "small"));
         daymenu.addProduct(new Food("Bacon burger", FoodType.BURGER, 395, 99.99, "regular"));
         daymenu.addProduct(new Food("Chicken wings", FoodType.FRIED, 420, 87.70, "small"));
         daymenu.addProduct(new Food("Chips", FoodType.FRIED, 120, 45.00, "medium"));
         daymenu.addProduct(new Food("Double Burger", FoodType.BURGER, 545, 112.00, "big"));
         daymenu.addProduct(new Food("Marshmallow Latte", FoodType.COFFEE, 123, 56.00, "big"));
-        daymenu.addProduct(new Food("Cola", FoodType.BEVERAGE, 57, 29.55, "small"));
-        daymenu.addProduct(new Food("Cola", FoodType.BEVERAGE, 78, 49.99, "big"));
+        daymenu.addProduct(new Food("Cola", FoodType.BEVERAGE, 57, 14.77, "small"));
+        daymenu.addProduct(new Food("Double Cola", FoodType.BEVERAGE, 78, 49.99, "big"));
+        daymenu.addProduct(new Food("Water", FoodType.BEVERAGE, 10, 23.99, "small"));
         System.out.println("1. Show available dishes(#no, category, name, price, calories, size):");
         daymenu.display(daymenu.getProductList());
 
@@ -30,9 +31,14 @@ public class Main {
         daymenu.filter(filtertype);
         daymenu.display(daymenu.getFilteredList());
 
+        /* Sorting available by price or by name ASC*/
         System.out.println("3. Show available dishes sorted by price:");
         daymenu.sortByPrice();
         daymenu.display(daymenu.getProductList());
+
+//        System.out.println("\n3. Show available dishes sorted by name:");
+//        daymenu.sortByName();
+//        daymenu.display(daymenu.getProductList());
 
         System.out.println("4. Search items by criteria:");
 
@@ -45,25 +51,28 @@ public class Main {
         param type Double - for the price (NEGATIVE - below price, POSITIVE - above price)
         param type Integer - for the calories (NEGATIVE - below calories value, POSITIVE - above calories value)
         param type String - for the size (equals or no)
+
+        to check different methods for different food fields change input parameter
          */
-        System.out.println("1) is one of");
-        Double priceValue = -30.00;
-        Boolean result = daymenu.checkOneOf(priceValue);
-        System.out.format("At least one of all food items price is < %3.2f : %b \n", Math.abs(priceValue), result);
+        Double priceValue = 100.43;
+        Integer caloriesValue = -601;
+        String sizeValue = "bigger";
 
-        System.out.println("2) is all");
-        Integer caloriesValue = 65;
-        result = daymenu.checkAll(caloriesValue);
-        System.out.format("All food items calories is > %3d : %b\n",  caloriesValue, result);
+        System.out.println("\n1) is one of");
+        System.out.format("At least one of all prices is > %3.2f : %b \n",
+                Math.abs(priceValue),
+                daymenu.checkOneOf(priceValue));
 
-        System.out.println("3) is none");
-        String sizeValue = "biggest";
-        result = daymenu.checkNone(sizeValue);
-        System.out.format("None of food items size is '%s' : %b\n", sizeValue, result);
+        System.out.println("\n2) is all");
+        System.out.format("All food items calories is < %3d : %b\n",
+                Math.abs(caloriesValue),
+                daymenu.checkAll(caloriesValue));
 
-        System.out.println("5. Get pair Name - Price of lowest item prices from every category, sorted by name:");
-        List<Food> lowestNamePricePerType = daymenu.getLowestPriceByType();
-        printNamePrice(lowestNamePricePerType);
+        System.out.println("\n3) is none");
+        System.out.format("None of food items size is '%s' : %b\n", sizeValue, daymenu.checkNone(sizeValue));
+
+        System.out.println("\n5. Get pair Name - Price of lowest item prices from every category, sorted by name:");
+        printNamePrice(daymenu.getLowestPriceByType());
 
     }
 
