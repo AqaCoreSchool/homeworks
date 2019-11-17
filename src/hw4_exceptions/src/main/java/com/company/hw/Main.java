3c57d16a6d9e7a5e26c5c9c6087ca4ebe919e9b9
Olya <35775825+Bilunska@users.noreply.github.com>
2019-11-17 21:09:35 +0200

    Update Main.java

--- src/hw4_exceptions/src/main/java/com/company/hw/Main.java
+++ src/hw4_exceptions/src/main/java/com/company/hw/Main.java
@@ -1,7 +1,7 @@
 package com.company.hw;
 
 import java.util.*;
-import com.company.hw.exceptions.NotAnyBooks;
+import com.company.hw.exceptions.NoSuchBookException;
 
 public class Main {
     public static void main(String[] args) {
@@ -23,12 +23,7 @@ public class Main {
                         System.out.print("[2] Enter author's Surname: Shevchenko or Bronte ->");
                         Scanner input = new Scanner(System.in);
                         String inpAuthor = input.next();
-                        try {
-                            olya.getFilteredByAuthor(inpAuthor);
-                        }
-                        catch(NotAnyBooks x){
-                            System.out.println("We don't have any Books by this Author");
-                        }
+                        olya.getFilteredByAuthor(inpAuthor);
                         break;
                     case 3:
                         System.out.println("[3] All books alphabetically by Book Name: ");
@@ -38,23 +33,13 @@ public class Main {
                         System.out.print("[4] Enter the year->");
                         Scanner input4 = new Scanner(System.in);
                         int inpYear = input4.nextInt();
-                        try {
-                            olya.hasEarlierBooks(inpYear);
-                        }
-                        catch(NotAnyBooks x){
-                            System.out.println("We have books only > 1603");
-                        }
+                        olya.hasEarlierBooks(inpYear);
                         break;
                     case 5:
                         System.out.print("[5] Enter Genre ->");
                         Scanner input5 = new Scanner(System.in);
                         String inpGenre = input5.next();
-                        try {
-                            olya.checkGenre(inpGenre);
-                        }
-                        catch(NotAnyBooks x){
-                            System.out.println("We don't have books in this Genre");
-                        }
+                        olya.checkGenre(inpGenre);
                         break;
 
                     case 6:
@@ -65,24 +50,12 @@ public class Main {
                         break;
                     case 7:
                         System.out.println("[7] Complicated Task:");
-                        try {
-                            olya.filteredByGenre();
-                        }
-                        catch(NotAnyBooks x){
-                            System.out.println("Hey, we don't have any Books now!");
-                        }
-
+                        olya.filteredByGenre();
                         break;
                     case 8:
                         System.out.println("[8] Map:");
-                        try {
-                            olya.useMap();
-                        }
-                        catch(NotAnyBooks x){
-                            System.out.println("Hey, we don't have any Books now!");
-                        }
+                        olya.useMap();
                         break;
-
                     default:
                         System.out.println("Woops. Hyston we have a problems. Try again with menu");
                         System.out.println("Enter Word to EXIT");
