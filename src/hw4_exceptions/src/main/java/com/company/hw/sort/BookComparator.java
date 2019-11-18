6132a8af9086d6e4e636c57f4b87c55a794c411a
Olya <35775825+Bilunska@users.noreply.github.com>
2019-11-18 15:23:18 +0200

    Update SortByBook.java 2

--- src/hw4_exceptions/src/main/java/com/company/hw/sort/BookComparator.java
+++ src/hw4_exceptions/src/main/java/com/company/hw/sort/BookComparator.java
@@ -3,7 +3,7 @@ package com.company.hw.sort;
 import com.company.hw.Library;
 
 import java.util.Comparator;
-public class SortByBook implements Comparator<Library> {
+public class BookComparator implements Comparator<Library> {
     public int compare(Library a, Library b) {
         return a.getBookName().compareTo(b.getBookName());
     }
