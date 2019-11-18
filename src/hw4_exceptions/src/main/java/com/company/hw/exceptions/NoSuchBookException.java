14a64903dc21ad94397bd69a80fe0f34fd504d2a
Olya <35775825+Bilunska@users.noreply.github.com>
2019-11-18 15:25:30 +0200

    Update NoSuchBookException.java

--- src/hw4_exceptions/src/main/java/com/company/hw/exceptions/NoSuchBookException.java
+++ src/hw4_exceptions/src/main/java/com/company/hw/exceptions/NoSuchBookException.java
@@ -1,6 +1,6 @@
 package com.company.hw.exceptions;
 
-public class NoSuchBookException extends Exception{
+public class NoSuchBookException extends RuntimeException{
     public NoSuchBookException(String message){
         super(message);
     }
