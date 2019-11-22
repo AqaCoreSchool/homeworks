dbedd2878b47ea7c1f0baa8fecc30405f3a9f985
Olya <35775825+Bilunska@users.noreply.github.com>
2019-11-22 20:58:26 +0200

    NotArtisticable.java clean

--- src/main/java/com/company/hw/interfaces/NotArtisticable.java
+++ src/main/java/com/company/hw/interfaces/NotArtisticable.java
@@ -1,6 +1,6 @@
 package com.company.hw.interfaces;
 
-public interface NotArtistic {
+public interface NotArtisticable {
 
     void isNotArtisticType(boolean isArtistic);
     int getNumberOfPages(int pages);
