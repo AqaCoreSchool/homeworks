4e17ba77776eaf5e66e31ed562895f307ef1e26a
Olya <35775825+Bilunska@users.noreply.github.com>
2019-11-22 17:38:30 +0200

    New PR

--- src/hw java8/main/java/com/company/hw/Main.java
+++ src/hw java8/main/java/com/company/hw/Main.java
@@ -5,62 +5,61 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        User olya = new User();
-        boolean shouldBreak = false;
+        User olyaUser = new User();
 
         Scanner myInput = new Scanner(System.in);
-        System.out.println("Here are 1-8 commands. \tEnter any word to EXIT \nEnter Number of task:");
+        System.out.println("Here are 1-8 commands. \tEnter 0 to EXIT \nEnter Number of task:");
         while (myInput.hasNextInt()) {
             int task = myInput.nextInt();
-            {
                 switch (task) {
                     case 1:
                         System.out.println("[1] All available: ");
-                        olya.printAvailableBooks();
+                        olyaUser.printAvailableBooks();
                         break;
                     case 2:
                         System.out.print("[2] Enter author's Surname: Shevchenko or Bronte ->");
                         Scanner input = new Scanner(System.in);
                         String inpAuthor = input.next();
-                        olya.filterBooksByAuthor(inpAuthor);
+                        olyaUser.filterBooksByAuthor(inpAuthor);
                         break;
                     case 3:
                         System.out.println("[3] All books alphabetically by Book Name: ");
-                        olya.sortAlpabetically();
+                        olyaUser.sortAlpabetically();
                         break;
                     case 4:
                         System.out.print("[4] Enter the year->");
                         Scanner input4 = new Scanner(System.in);
                         int inpYear = input4.nextInt();
-                        olya.findEarlierBooks(inpYear);
+                        olyaUser.findEarlierBooks(inpYear);
                         break;
                     case 5:
                         System.out.print("[5] Enter Genre ->");
                         Scanner input5 = new Scanner(System.in);
                         String inpGenre = input5.next();
-                        olya.checkGenre(inpGenre);
+                        olyaUser.checkGenre(inpGenre);
                         break;
 
                     case 6:
                         System.out.print("[6] Enter Author ->");
                         Scanner input6 = new Scanner(System.in);
                         String inpString = input6.next();
-                        System.out.println(olya.hasNoneBooksByAuthor(inpString));
+                        System.out.println(olyaUser.hasNoneBooksByAuthor(inpString));
                         break;
                     case 7:
                         System.out.println("[7] Complicated Task:");
-                        olya.printFilteredBooksByGenre();
+                        olyaUser.printFilteredBooksByGenre();
                         break;
                     case 8:
                         System.out.println("[8] Map:");
-                        olya.useMap();
+                        olyaUser.useMap();
+                        break;
+                    case 0:
+                        System.exit(0);
                         break;
                     default:
                         System.out.println("Woops. Hyston we have a problems. Try again with menu");
-                        System.out.println("Enter Word to EXIT");
+                        System.out.println("Enter 0 to EXIT");
                 }
-                if (shouldBreak) break;
-            }
         }
     }
 }
