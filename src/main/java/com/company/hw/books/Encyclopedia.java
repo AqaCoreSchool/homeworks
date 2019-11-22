29ad98148246d0494501e041bd772192141973dd
Olya <35775825+Bilunska@users.noreply.github.com>
2019-11-22 20:57:23 +0200

    Update Encyclopedia.java clean

--- src/main/java/com/company/hw/books/Encyclopedia.java
+++ src/main/java/com/company/hw/books/Encyclopedia.java
@@ -1,9 +1,9 @@
 package com.company.hw.books;
 
 import com.company.hw.Library;
-import com.company.hw.interfaces.NotArtistic;
+import com.company.hw.interfaces.NotArtisticable;
 
-public class Encyclopedia extends Library implements NotArtistic {
+public class Encyclopedia extends Library implements NotArtisticable {
 
     public Encyclopedia(String author, String name, int year, boolean avail) {
         super(author, name, year, avail);
