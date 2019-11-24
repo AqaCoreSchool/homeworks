368ce5be5a92619f3483c8147fe44c7295c4346e
Andrii Rybka <r.andy@ukr.net>
2019-11-25 00:29:35 +0200

    Updated task

--- src/main/java/task/Main.java
+++ src/main/java/task/Main.java
@@ -1,51 +1,87 @@
 package task;
 
+import com.github.javafaker.Faker;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     private static final String TEMPLATE = "Name: %s, salary: %s, balance: %s";
+    private static List<Person> people = generatePeople();
+    private static List<Person> peopleWithSuspiciousBankAccounts = new ArrayList<>();
+    private static List<Person> peopleWithSuspiciousBankAccountsWithLambda = new ArrayList<>();
 
     public static void main(String[] args) {
-        List<Person> people = DataGenerator.generatePeople();
-        List<Person> peopleWithSuspiciousBankAccounts = new ArrayList<>();
 
+        addPeopleWithSuspiciousBankAccounts();
+        addPeopleWithSuspiciousBankAccountsWithLambda();
+
+        String result = getPeopleWithSuspiciousBankAccounts();
+        String result1 = getPeopleWithSuspiciousBankAccountsWithLambda();
+
+        System.out.println(result);
+        System.out.println("====================================");
+        System.out.println(result1);
+        System.out.println("Usual and lambda are equal: " + result.equals(result1));
+
+    }
+
+    private static List<Person> generatePeople() {
+        Faker faker = new Faker();
+        return IntStream.rangeClosed(1, 1000).mapToObj(i -> {
+            int age = faker.number().numberBetween(18, 60);
+            double salary = faker.number().randomDouble(2, 100, 10000);
+            Person person = new Person(faker.name().fullName(), age, salary);
+
+            boolean isActive = faker.bool().bool();
+            double balance = isActive ? faker.number().randomDouble(2, 100, 50000) : 0;
+            int weeksAgo = faker.number().numberBetween(4, 26);
+            person.setBankAccount(new BankAccount(faker.company().name(), balance, isActive, LocalDate.now().minusWeeks(weeksAgo)));
+            return person;
+        }).collect(Collectors.toList());
+    }
+
+    private static void addPeopleWithSuspiciousBankAccounts() {
+        // usual
+        for (int i = 0; i < people.size(); i++) {
+            Person person = people.get(i);
+            if (person.getBankAccount().isActive() && person.getBankAccount().getBalance() > person.getSalary()
+                    && getIncomeSinceAccountStartDate(person) > person.getBankAccount().getBalance()) {
+                peopleWithSuspiciousBankAccounts.add(people.get(i));
+            }
+        }
+    }
+
+    private static void addPeopleWithSuspiciousBankAccountsWithLambda() {
+        // java 8
         people.stream()
                 .filter(person -> person.getBankAccount().isActive()
                         && person.getBankAccount().getBalance() > person.getSalary())
                 .filter(person -> getIncomeSinceAccountStartDate(person) > person.getBankAccount().getBalance())
-                .forEach(peopleWithSuspiciousBankAccounts::add);
+                .forEach(peopleWithSuspiciousBankAccountsWithLambda::add);
+    }
 
-        for (int i = 0; i < people.size(); i++) {
-            Person person = people.get(i);
-            if (person.getBankAccount().isActive() && person.getBankAccount().getBalance() > person.getSalary()) {
-                if (getIncomeSinceAccountStartDate(person) <= person.getBankAccount().getBalance()) {
-                    continue;
-                } else {
-                    peopleWithSuspiciousBankAccounts.add(people.get(i));
-                }
-            }
-        }
-
-        String result = "";
-        result = peopleWithSuspiciousBankAccounts.stream().map(Main::fillInTemplateRow).peek(result::concat)
-                .collect(Collectors.joining("\n"));
-        System.out.println(result);
-
+    private static String getPeopleWithSuspiciousBankAccounts() {
+        // usual
         String result1 = "";
         for (Person person : peopleWithSuspiciousBankAccounts) {
             result1 = result1 + fillInTemplateRow(person);
             result1 += "\n";
         }
-        result1 = result1.replaceAll("\\n$", "");
-        System.out.println("====================================");
-        System.out.println(result1);
-        System.out.println("Is equal: " + result1.equals(result));
+        return result1.replaceAll("\\n$", "");
+    }
 
+    private static String getPeopleWithSuspiciousBankAccountsWithLambda() {
+        // java 8
+        String result = "";
+        result = peopleWithSuspiciousBankAccountsWithLambda.stream().map(Main::fillInTemplateRow).peek(result::concat)
+                .collect(Collectors.joining("\n"));
+        return result;
     }
 
     private static double getIncomeSinceAccountStartDate(Person person) {
