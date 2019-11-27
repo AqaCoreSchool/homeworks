7d90ae0e1d23c2d425d185cbaa5dee3fc2c01b3d
nkfortest <nazariy.kushnir@gmail.com>
2019-11-27 23:41:14 +0200

    - manage webdriver setup by WebdriverManager  - add driver.get to BeforeMethod  - set login, creating punchInOut, checkRecords flows to static parametrized methods inside test  - added second test method to test empty record on weeks before-after (* task)

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -15,17 +16,22 @@ import java.util.concurrent.TimeUnit;
 
 public class SeleniumTest {
     private WebDriver driver;
-    private final String USERNAME = "TestUser01";
+    private final String USERNAME = "TestUser06";
     private final String PASSWORD = "Vfylhfujhf!1";
     private final String START_MESSAGE = "Start work TEST!";
     private final String END_MESSAGE = "Stop work TEST!";
     private final String TODAY_DATE = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    private final String WEEK_BEFORE = LocalDateTime.now().minusWeeks(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    private final String WEEK_AFTER = LocalDateTime.now().plusWeeks(1).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+    private final String URL = "http://test.biz.ua";
+
 
     @BeforeMethod
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        WebDriverManager.chromedriver().version("77.0.3865.40").setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get(URL);
     }
 
     @AfterMethod
@@ -33,56 +39,91 @@ public class SeleniumTest {
         driver.quit();
     }
 
-    @Test
+    @Test(priority = 1)
     public void createPunchAndCheckRecordDataTest(){
-        driver.get("http://test.biz.ua");
+        login(driver, USERNAME, PASSWORD);
+        String punchInDateTime = createPunchInOutAndGetCreationDate(driver, START_MESSAGE);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        String punchOutDateTime = createPunchInOutAndGetCreationDate(driver, END_MESSAGE);
+        List<WebElement> latestRecordBody = getLastRecord(driver, TODAY_DATE);
+        String recordStartDateTime = latestRecordBody.get(1).getText();
+        String recordPunchInNote = latestRecordBody.get(2).getText();
+        String recordEndDateTime = latestRecordBody.get(3).getText();
+        String recordPunchOutNote = latestRecordBody.get(4).getText();
+        Assert.assertEquals(recordStartDateTime, punchInDateTime,
+                "record Punch In datetime is different then actual Punch In datetime!");
+        Assert.assertEquals(recordPunchInNote, START_MESSAGE,
+                "record Punch In note is different then actual Punch In note!");
+        Assert.assertEquals(recordEndDateTime, punchOutDateTime,
+                "record Punch Out datetime is different then actual Punch Out datetime!");
+        Assert.assertEquals(recordPunchOutNote, END_MESSAGE,
+                "record Punch Out datetime is different then actual Punch Out datetime!");
+    }
+
+    @Test(priority = 2)
+    public void checkRecordsTableIsEmpty(){
+        login(driver, USERNAME, PASSWORD);
+        WebElement noRecordsWeekAfter = getLastRecordEmpty(driver, WEEK_AFTER);
+        Assert.assertTrue(noRecordsWeekAfter.isDisplayed());
+        WebElement noRecordsWeekBefore = getLastRecordEmpty(driver, WEEK_BEFORE);
+        Assert.assertTrue(noRecordsWeekBefore.isDisplayed());
+    }
+
+    public static void login(WebDriver driver, String username, String password){
         WebElement usernameField = driver.findElement(By.id("txtUsername"));
-        usernameField.sendKeys(USERNAME);
+        usernameField.sendKeys(username);
         WebElement passwordField = driver.findElement(By.id("txtPassword"));
-        passwordField.sendKeys(PASSWORD);
+        passwordField.sendKeys(password);
         WebElement loginButton = driver.findElement(By.id("btnLogin"));
         loginButton.click();
+    }
+
+    public static String createPunchInOutAndGetCreationDate(WebDriver driver, String message){
         WebElement timeSection = driver.findElement(By.id("menu_time_viewTimeModule"));
         timeSection.click();
         WebElement attendanceMenu =  driver.findElement(By.id("menu_attendance_Attendance"));
         attendanceMenu.click();
         WebElement punchInOut =  driver.findElement(By.id("menu_attendance_punchIn"));
         punchInOut.click();
-        WebElement startDate = driver.findElement(By.id("currentDate"));
-        WebElement startTime = driver.findElement(By.id("currentTime"));
-        String punchInDateTime = String.format("%s %s:00 GMT 2", startDate.getText(), startTime.getText());
+        WebElement punchDate = driver.findElement(By.id("currentDate"));
+        WebElement punchTime = driver.findElement(By.id("currentTime"));
+        String punchDateTime = String.format("%s %s:00 GMT 2", punchDate.getText(), punchTime.getText());
         WebElement inTextArea = driver.findElement(By.xpath("//textarea[@id='note']"));
-        inTextArea.sendKeys(START_MESSAGE);
-        WebElement buttonPunchIn = driver.findElement(By.id("btnPunch"));
-        buttonPunchIn.click();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        WebElement endDate = driver.findElement(By.id("currentDate"));
-        WebElement endTime = driver.findElement(By.id("currentTime"));
-        String punchOutDateTime = String.format("%s %s:00 GMT 2", endDate.getText(), endTime.getText());
-        WebElement outTextArea = driver.findElement(By.xpath("//textarea[@id='note']"));
-        outTextArea.sendKeys(END_MESSAGE);
-        WebElement buttonPunchOut = driver.findElement(By.id("btnPunch"));
-        buttonPunchOut.click();
-        attendanceMenu =  driver.findElement(By.id("menu_attendance_Attendance"));
+        inTextArea.sendKeys(message);
+        WebElement buttonConfirmPunch = driver.findElement(By.id("btnPunch"));
+        buttonConfirmPunch.click();
+        return punchDateTime;
+    }
+
+    public static List<WebElement> getLastRecord(WebDriver driver, String date){
+        WebElement attendanceMenu =  driver.findElement(By.id("menu_attendance_Attendance"));
         attendanceMenu.click();
         WebElement myRecords =  driver.findElement(By.id("menu_attendance_viewMyAttendanceRecord"));
         myRecords.click();
         WebElement dateField =  driver.findElement(By.id("attendance_date"));
         dateField.clear();
-        dateField.sendKeys(TODAY_DATE + Keys.ENTER);
+        dateField.sendKeys(date + Keys.ENTER);
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         List<WebElement> tableRows = driver.findElements(By.xpath(
-                "//form[@id='employeeRecordsForm']/table/tbody/tr[@class='odd' or @class='even']"));
+                "//form[@id='employeeRecordsForm']//tr[@class='odd' or @class='even']"));
         WebElement latestRecord = tableRows.get(tableRows.size()-1);
-        List<WebElement> latestRecordBody = latestRecord.findElements(By.xpath("./td"));
-        String recordStartDateTime = latestRecordBody.get(1).getText();
-        String recordPunchInNote = latestRecordBody.get(2).getText();
-        String recordEndDateTime = latestRecordBody.get(3).getText();
-        String recordPunchOutNote = latestRecordBody.get(4).getText();
-        Assert.assertEquals(recordStartDateTime, punchInDateTime, "record Punch In datetime is different then actual Punch In datetime!");
-        Assert.assertEquals(recordPunchInNote, START_MESSAGE, "record Punch In note is different then actual Punch In note!");
-        Assert.assertEquals(recordEndDateTime, punchOutDateTime, "record Punch Out datetime is different then actual Punch Out datetime!");
-        Assert.assertEquals(recordPunchOutNote, END_MESSAGE, "record Punch Out datetime is different then actual Punch Out datetime!");
+        return latestRecord.findElements(By.xpath("./td"));
+    }
+
+    public static WebElement getLastRecordEmpty(WebDriver driver, String date){
+        WebElement timeSection = driver.findElement(By.id("menu_time_viewTimeModule"));
+        timeSection.click();
+        WebElement attendanceMenu =  driver.findElement(By.id("menu_attendance_Attendance"));
+        attendanceMenu.click();
+        WebElement myRecords =  driver.findElement(By.id("menu_attendance_viewMyAttendanceRecord"));
+        myRecords.click();
+        WebElement dateField =  driver.findElement(By.id("attendance_date"));
+        dateField.clear();
+        dateField.sendKeys(date + Keys.ENTER);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        List<WebElement> noRecordRow = driver.findElements(By.xpath(
+                "//form[@id='employeeRecordsForm']//tr/td[@id='noRecordsColumn']"));
+        return noRecordRow.get(0);
     }
 
 }
