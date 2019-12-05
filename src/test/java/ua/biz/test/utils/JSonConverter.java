99334ad29e2d023fdbf83a5db1b712977254bd7c
Ivan <MWIvanko@gmail.com>
2019-12-06 00:20:25 +0200

    Some changes

--- src/test/java/ua/biz/test/utils/JSonConverter.java
+++ src/test/java/ua/biz/test/utils/JSonConverter.java
@@ -5,13 +5,4 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSonConverter<T> {
 
-    private ObjectMapper mapper = new ObjectMapper();
-
-    public String convertObjectToJson(T t) throws JsonProcessingException {
-            return mapper.writeValueAsString(t);
-    }
-
-    public T convertJsonToObject(String json, Class<T> ob) throws JsonProcessingException {
-           return mapper.readerFor(ob).readValue(json);
-    }
 }
