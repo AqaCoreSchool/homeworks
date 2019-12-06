2236bcb147377d9fdd610eabe4a2ab9a011c297f
51stRoR <51sttestinrror@gmail.com>
2019-12-06 10:49:36 +0200

    - change to manual JSON strings creating

--- src/test/java/test/JsonTest.java
+++ src/test/java/test/JsonTest.java
@@ -1,6 +1,5 @@
 package test;
 
-import com.google.gson.Gson;
 import data.CandidateData;
 import data.LocationData;
 import data.VacancyData;
@@ -12,13 +11,34 @@ public class JsonTest {
 
     @Test
     public void checkJsonDeserialization() {
-        Gson jsonMaker = new Gson();
         CandidateData candidate = new CandidateData("test", "Nazariy Kushnir", "2019-12-05");
         LocationData location = new LocationData();
         VacancyData vacancy = new VacancyData("Test", "Nazariy Kushnir");
-        String candidateJson = jsonMaker.toJson(candidate);
-        String locationJson = jsonMaker.toJson(location);
-        String vacancyJson = jsonMaker.toJson(vacancy);
+        String candidateJson = String.format("{\"firstName\":\"%s\",\"lastName\":\"%s\",\"email\":\"%s\",\"contactNo\":\"%s\",\"vacancyName\":\"%s\",\"hiringManager\":\"%s\",\"comment\":\"%s\",\"date\":\"%s\"}\n",
+                candidate.getFirstName(),
+                candidate.getLastName(),
+                candidate.getEmail(),
+                candidate.getContactNo(),
+                candidate.getVacancyName(),
+                candidate.getHiringManager(),
+                candidate.getComment(),
+                candidate.getDate());
+        String locationJson = String.format("{\"name\":\"%s\",\"countryCode\":\"%s\",\"state\":\"%s\",\"city\":\"%s\",\"address\":\"%s\",\"zip\":\"%s\",\"phone\":\"%s\",\"fax\":\"%s\",\"notes\":\"%s\"}",
+                location.getName(),
+                location.getCountryCode(),
+                location.getState(),
+                location.getCity(),
+                location.getAddress(),
+                location.getZip(),
+                location.getPhone(),
+                location.getFax(),
+                location.getNotes());
+        String vacancyJson = String.format("{\"jobTitleOption\":%d,\"vacancyName\":\"%s\",\"hiringManager\":\"%s\",\"positions\":\"%s\",\"description\":\"%s\"}",
+                vacancy.getJobTitleOption(),
+                vacancy.getVacancyName(),
+                vacancy.getHiringManager(),
+                vacancy.getPositions(),
+                vacancy.getDescription());
         Assert.assertEquals(candidate, JsonReader.readCandidateFromJson(candidateJson));
         Assert.assertEquals(location, JsonReader.readLocationFromJson(locationJson));
         Assert.assertEquals(vacancy, JsonReader.readVacancyFromJson(vacancyJson));
