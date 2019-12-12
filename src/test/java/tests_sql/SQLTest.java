e4aaab94b765a2ba31f29a16fead5eabfb993261
Andrii Shtyiuk <andriy.shtyiuk@gmail.com>
2019-12-12 16:44:49 +0200

    WIP

---
+++ src/test/java/tests_sql/SQLTest.java
@@ -0,0 +1,23 @@
+package sql.tests_sql;
+
+import data.LoginInfo;
+import data.Vacancy;
+import org.testng.annotations.Test;
+import pages.VacanciesPage;
+import tests.BaseTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SQLTest extends BaseTest {
+
+    @Test
+    public void testSQL(){
+        Vacancy vacancy = new Vacancy();
+        VacanciesPage vacanciesPage = loginPage
+                .loginIntoSite(LoginInfo.LOGIN, LoginInfo.PASSWORD)
+                .moveToVacanciesPage()
+                .addNewVacancy(vacancy);
+
+        assertThat(vacanciesPage.findVacancy(vacancy)).isEqualTo()
+    }
+}
